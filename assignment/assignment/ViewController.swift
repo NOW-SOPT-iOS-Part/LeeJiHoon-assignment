73350bac787d8b248e2a36f56886ed433cb12186
hooni <jhlee909090@gmail.com>
2024-04-09 11:52:23 +0900

    feat: 다크모드 고정

--- assignment/assignment/ViewController.swift
+++ assignment/assignment/ViewController.swift
@@ -11,21 +11,57 @@ import Then
 
 class ViewController: UIViewController {
 
-    let LoginLabel = UILabel().then {
-        $0.text = "TVING ID 로그인"
-        $0.textColor = UIColor(named: "gray84")
-        
-    }
-    
+    let loginLabel = UILabel().then {
+          $0.text = "TVING ID 로그인"
+          $0.textColor = UIColor(named: "gray84")
+      }
+      
+      let idTextFieldView = UIView().then {
+          $0.backgroundColor = .white
+          $0.layer.cornerRadius = 10
+      }
+      
+      let passwordTextFieldView = UIView().then {
+          $0.backgroundColor = .white
+          $0.layer.cornerRadius = 10
+      }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .brown
+        view.backgroundColor = .black
+        addSubViews()
+        setConstraints()
+        }
+
+        func addSubViews() {
+            view.addSubview(loginLabel)
+            view.addSubview(idTextFieldView)
+            view.addSubview(passwordTextFieldView)
+        }
+        
+        func setConstraints() {
+            loginLabel.snp.makeConstraints { make in
+                make.top.equalTo(view.safeAreaLayoutGuide).offset(100)
+                make.centerX.equalTo(view)
+            }
+            
+            idTextFieldView.snp.makeConstraints { make in
+                make.top.equalTo(loginLabel.snp.bottom).offset(20)
+                make.centerX.equalTo(view)
+                make.width.equalTo(335)
+                make.height.equalTo(52)
+            }
+            
+            passwordTextFieldView.snp.makeConstraints { make in
+                make.top.equalTo(idTextFieldView.snp.bottom).offset(20)
+                make.centerX.equalTo(view)
+                make.width.equalTo(335)
+                make.height.equalTo(52)
+            }
+        }
     }
 
-
-}
 //
 //#Preview{
 //    ViewController()
