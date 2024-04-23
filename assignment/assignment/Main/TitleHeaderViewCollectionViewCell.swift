2500f2a149949eadc33b35c0b64f408276fc017b
hooni <jhlee909090@gmail.com>
2024-04-23 20:40:53 +0900

    feat: headerVIew Cell UI 구성완료

--- assignment/assignment/Main/TitleHeaderViewCollectionViewCell.swift
+++ assignment/assignment/Main/TitleHeaderViewCollectionViewCell.swift
@@ -11,52 +11,76 @@ import SnapKit
 
 class TitleHeaderViewCollectionViewCell: UICollectionViewCell {
     
-    static let identifier = "TitleHeaderViewCollectionViewCell"
+    
+      static let identifier = "TitleHeaderViewCollectionViewCell"
 
-    
-    private let titleLabel = UILabel().then {
-        $0.textColor = .white
-        $0.font = UIFont(name: "Pretendard-SemiBold", size: 15)
-    }
-    
-    private let subtitleLabel = UILabel().then {
-        $0.textColor = .white
-        $0.font = UIFont(name: "Pretendard-Medium", size: 12)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError()
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        self.backgroundColor = .black
-        
-        self.contentView.addSubview(self.titleLabel)
-        self.contentView.addSubview(self.subtitleLabel)
-        
-        self.titleLabel.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(10)
-            $0.left.equalToSuperview().offset(10)
-        }
-        
-        self.subtitleLabel.snp.makeConstraints {
-            $0.top.equalTo(self.titleLabel.snp.bottom).offset(5)
-            $0.left.equalToSuperview().offset(10)
-        }
-    }
-    
-    
-    func prepare(titleText: String?, subtitleText: String?) {
-        
-        if let titleText = titleText {
-            self.titleLabel.text = titleText
-        }
-        
-        if let subtitleText = subtitleText {
-            self.subtitleLabel.text = subtitleText
-        }
-      self.titleLabel.text = titleText
-    }
-  
+      private let titleLabel = UILabel().then {
+          $0.textColor = .white
+          $0.font = UIFont(name: "Pretendard-SemiBold", size: 23)
+      }
+      
+      private let subtitleLabel = UILabel().then {
+          $0.textColor = .white
+          $0.font = UIFont(name: "Pretendard-Medium", size: 12)
+      }
+      
+      private let arrowIcon = UIImageView().then {
+          
+          //이미지 sf심볼로 대체
+          $0.image = UIImage(systemName: "chevron.right")
+          $0.tintColor = .white
+          $0.contentMode = .scaleAspectFit
+      }
+      
+      private let subtitleStackView = UIStackView().then {
+          $0.axis = .horizontal
+          $0.spacing = 1
+          $0.alignment = .center
+          $0.distribution = .fill
+      }
+      
+      required init?(coder: NSCoder) {
+          fatalError("init(coder:) has not been implemented")
+      }
+      
+      override init(frame: CGRect) {
+          super.init(frame: frame)
+          setupViews()
+          setupConstraints()
+      }
+      
+      private func setupViews() {
+          backgroundColor = .black
+          
+          contentView.addSubview(titleLabel)
+          contentView.addSubview(subtitleStackView)
+          
+          subtitleStackView.addArrangedSubview(subtitleLabel)
+          subtitleStackView.addArrangedSubview(arrowIcon)
+      }
+      
+      private func setupConstraints() {
+          titleLabel.snp.makeConstraints {
+              $0.top.equalToSuperview().offset(10)
+              $0.left.equalToSuperview().offset(10)
+          }
+          
+          subtitleStackView.snp.makeConstraints {
+              $0.top.equalToSuperview().offset(10)
+              $0.trailing.equalToSuperview().offset(-10)
+          }
+          
+          arrowIcon.snp.makeConstraints {
+              $0.width.equalTo(12)
+              $0.height.equalTo(12)
+          }
+      }
+      
+      func prepare(titleText: String?, subtitleText: String?) {
+          titleLabel.text = titleText
+          
+          if let subtitleText = subtitleText {
+              subtitleLabel.text = subtitleText
+          }
+      }
 }
